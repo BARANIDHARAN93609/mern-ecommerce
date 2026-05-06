894c2704097e11d3162ac54549f34e8095fc50b3
Baranidharan M <baranidharan2003yt@gmail.com>
2026-05-06 13:00:47 +0530

    added root route fix

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,9 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
-
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
 app.get("/api/health", (req, res) =>
   res.json({ status: "OK", timestamp: new Date().toISOString() }),
 );
